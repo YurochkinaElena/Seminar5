d7bd12ae782c0dc433a54b91b5aa7971579ba2e0
yurochkina.elena <transnab@mail.ru>
2022-11-29 13:16:18 +0300

    ДЗ задача 34

---
+++ task34(2)/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите количество элементов массива:\t");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [number];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"{"Количество четных чисел:\t", 1}{FindNumberEver(array)}");
+
+int FindNumberEver(int[] arr)
+{
+    int amounts = 0;
+    foreach (int item in arr)
+    {
+        if (item % 2 == 0)
+        {
+            amounts++;
+        }
+    }
+    return amounts;
+}
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length -1)
+        {
+            Console.WriteLine($"{arr[1]}, ");
+        }
+        else
+        {
+            Console.Write($"{arr[i]}, ");
+        }
+    }
+}
